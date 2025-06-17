7ad81ea42582e4feb21e8e279a476dd79c41cdee
manishbhilare <manishbhilare12@gmail.com>
2025-06-17 14:17:31 +0530

    initial commit

---
+++ src/main/java/com/example/splitwiseclone/SplitwisecloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitwiseclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwisecloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwisecloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/splitwiseclone/SplitwisecloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splitwiseclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwisecloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
